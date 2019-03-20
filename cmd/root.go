b9c3a887dbaa49e3dd28b7b6796ab24bc8b3f6e2
David Michael <david.michael@gmail.com>
2019-03-20 15:28:26 -0400

    system status

--- cmd/root.go
+++ cmd/root.go
@@ -83,8 +83,8 @@ func initConfig() {
 		}
 
 		// Search config in home directory with name ".blackbox-cli" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".blackbox")
+		viper.AddConfigPath(home + "/.crypdex")
+		viper.SetConfigName("blackbox")
 	}
 
 	viper.AutomaticEnv() // read in environment variables that match
@@ -108,7 +108,7 @@ func initClient() {
 		host = viper.GetString("host")
 	}
 
-	blackboxClient, err = blackbox.NewClient(host)
+	blackboxClient, err = blackbox.NewClient(host, viper.GetString("token"))
 	if err != nil {
 		fatal(err)
 	}
